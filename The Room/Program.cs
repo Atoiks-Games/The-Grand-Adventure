dd62aefd5a389bc501da6226c43e5210ffe2ff82
PerenDarksbane <ajbimm@gmail.com>
2016-07-22 11:27:18 -0400

    Making things classy.

--- The Room/Program.cs
+++ The Room/Program.cs
@@ -7,10 +7,41 @@ using static System.Console;
 
 namespace The_Room
 {
+    class Race
+    {
+        public static string raceName;
+
+    }
+    class Class
+    {
+        public static string className;
+
+    }
+    class Scores
+    {
+        public static int str;
+        public static int dex;
+        public static int con;
+        public static int intel;
+        public static int wis;
+        public static int cha;
+        public static int playerNumber;
+
+        public static void DisplayScores()
+        {
+            WriteLine("Your strength is: " + str);
+            WriteLine("Your dexterity is: " + dex);
+            WriteLine("Your constitution is: " + con);
+            WriteLine("Your intelligence is: " + intel);
+            WriteLine("Your wisdom is: " + wis);
+            WriteLine("Your charisma is: " + cha);
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+
             const int fudgeFactor = 1;
             string nameFirst = "";
             string nameLast = "";
@@ -24,20 +55,15 @@ namespace The_Room
             int damageMod = 0;
 
             Random rnd = new Random();
-            int strength = rnd.Next(1, 6) + rnd.Next(1, 6) + rnd.Next(1, 6) + fudgeFactor;
-            int dexterity = rnd.Next(1, 6) + rnd.Next(1, 6) + rnd.Next(1, 6) + fudgeFactor;
-            int constitution = rnd.Next(1, 6) + rnd.Next(1, 6) + rnd.Next(1, 6) + fudgeFactor;
-            int intelligence = rnd.Next(1, 6) + rnd.Next(1, 6) + rnd.Next(1, 6) + fudgeFactor;
-            int wisdom = rnd.Next(1, 6) + rnd.Next(1, 6) + rnd.Next(1, 6) + fudgeFactor;
-            int charisma = rnd.Next(1, 6) + rnd.Next(1, 6) + rnd.Next(1, 6) + fudgeFactor;
+            Scores.str = rnd.Next(1, 6) + rnd.Next(1, 6) + rnd.Next(1, 6) + fudgeFactor;
+            Scores.dex = rnd.Next(1, 6) + rnd.Next(1, 6) + rnd.Next(1, 6) + fudgeFactor;
+            Scores.con = rnd.Next(1, 6) + rnd.Next(1, 6) + rnd.Next(1, 6) + fudgeFactor;
+            Scores.intel = rnd.Next(1, 6) + rnd.Next(1, 6) + rnd.Next(1, 6) + fudgeFactor;
+            Scores.wis = rnd.Next(1, 6) + rnd.Next(1, 6) + rnd.Next(1, 6) + fudgeFactor;
+            Scores.cha = rnd.Next(1, 6) + rnd.Next(1, 6) + rnd.Next(1, 6) + fudgeFactor;
 
             WriteLine("Welcome To:\n\n _The Grand Adventure_ \n\n Let's begin by determining your character's ability rankings and figuring out your name!");
-            WriteLine("Your strength is: " + strength);
-            WriteLine("Your dexterity is: " + dexterity);
-            WriteLine("Your constitution is: " + constitution);
-            WriteLine("Your intelligence is: " + intelligence);
-            WriteLine("Your wisdom is: " + wisdom);
-            WriteLine("Your charisma is: " + charisma);
+            Scores.DisplayScores();
 
             while (nameCheck != "y") {
                 WriteLine("What is your first name adventurer? ");
@@ -64,61 +90,56 @@ namespace The_Room
             switch (race)
             {
                 case "d":
-                    constitution = constitution + 2;
-                    strength = strength + 1;
-                    dexterity = dexterity - 1;
-                    race = "Dwarf";
+                    Scores.con = Scores.con + 2;
+                    Scores.str = Scores.str + 1;
+                    Scores.dex = Scores.dex - 1;
+                    Race.raceName = "Dwarf";
                     WriteLine("You are now a dwarf! Diggy Diggy Hole!");
                     break;
                 case "h":
-                    charisma = charisma + 2;
-                    strength = strength + 1;
-                    wisdom = wisdom - 1;
-                    race = "Human";
+                    Scores.cha = Scores.cha + 2;
+                    Scores.str = Scores.str + 1;
+                    Scores.wis = Scores.wis - 1;
+                    Race.raceName = "Human";
                     WriteLine("You are now a human! Yay!");
                     break;
                 case "e":
-                    intelligence = intelligence + 2;
-                    dexterity = dexterity + 1;
-                    constitution = constitution - 1;
-                    race = "Elf";
+                    Scores.intel = Scores.intel + 2;
+                    Scores.dex = Scores.dex + 1;
+                    Scores.con = Scores.con - 1;
+                    Race.raceName = "Elf";
                     WriteLine("You are now an elf! A very popular choice...");
                     break;
                 case "g":
-                    dexterity = dexterity + 2;
-                    charisma = charisma + 1;
-                    strength = strength - 1;
-                    race = "Gnome";
+                    Scores.dex = Scores.dex + 2;
+                    Scores.cha = Scores.cha + 1;
+                    Scores.str = Scores.str - 1;
+                    Race.raceName = "Gnome";
                     WriteLine("You are now a gnome! How cute!");
                     break;
                 case "i":
-                    wisdom = wisdom + 2;
-                    intelligence = intelligence + 1;
-                    charisma = charisma - 1;
-                    race = "Infernal";
+                    Scores.wis = Scores.wis + 2;
+                    Scores.intel = Scores.intel + 1;
+                    Scores.cha = Scores.cha - 1;
+                    Race.raceName = "Infernal";
                     WriteLine("You are now an infernal! Is it me or is it getting hot in here?");
                     break;
                 case "l":
-                    strength = strength + 2;
-                    constitution = constitution + 1;
-                    charisma = charisma - 1;
-                    race = "Lizardfolk";
+                    Scores.str = Scores.str + 2;
+                    Scores.con = Scores.con + 1;
+                    Scores.cha = Scores.cha - 1;
+                    Race.raceName = "Lizardfolk";
                     WriteLine("You are now a lizardfolk. Hisssssssss...");
                     break;
                 default:
                 WriteLine("Ye dun goofed!");
                 goto Race;
             }
-        
-            WriteLine("Your new strength is: " + strength);
-            WriteLine("Your new dexterity is: " + dexterity);
-            WriteLine("Your new constitution is: " + constitution);
-            WriteLine("Your new intelligence is: " + intelligence);
-            WriteLine("Your new wisdom is: " + wisdom);
-            WriteLine("Your new charisma is: " + charisma);
 
-        AdventuringClass:
-            WriteLine("Great! Your name is: {0} {1}. You race is: {2}. Now what class of adventurer are you?", nameFirst, nameLast, race);
+            Scores.DisplayScores();
+
+            AdventuringClass:
+            WriteLine("Great! Your name is: {0} {1}. You race is: {2}. Now what class of adventurer are you?", nameFirst, nameLast, Race.raceName);
             WriteLine("Again, write the first letter of the class you want!");
             WriteLine("Warrior");
             WriteLine("Ranger");
@@ -128,44 +149,44 @@ namespace The_Room
             switch (adventuringClass)
             {
                 case "w":
-                    constitution = constitution + 1;
-                    strength = strength + 1;
-                    adventuringClass = "Warrior";
+                    Scores.con = Scores.con + 1;
+                    Scores.str = Scores.str + 1;
+                    Class.className = "Warrior";
                     WriteLine("You are a warrior! Enemies shall fear your mighty sword!");
-                    resistance = constitution + level;
-                    damageMod = (strength - 10) * level;
+                    resistance = Scores.con + level;
+                    damageMod = (Scores.con - 10) * level;
                     break;
                 case "r":
-                    wisdom = wisdom + 1;
-                    dexterity = dexterity + 1;
-                    adventuringClass = "Ranger";
+                    Scores.wis = Scores.wis + 1;
+                    Scores.dex = Scores.dex + 1;
+                    Class.className = "Ranger";
                     WriteLine("You are now a ranger! Enemies shall fear your piercing arrows!");
-                    resistance = dexterity + level;
-                    damageMod = (strength - 10) * level;
+                    resistance = Scores.dex + level;
+                    damageMod = (Scores.str - 10) * level;
                     break;
                 case "m":
-                    intelligence = intelligence + 1;
-                    wisdom = wisdom + 1;
-                    adventuringClass = "Mage";
+                    Scores.intel = Scores.intel + 1;
+                    Scores.wis = Scores.wis + 1;
+                    Class.className = "Mage";
                     WriteLine("You are now a mage! Enemies shall fear your arcanic powers!");
-                    resistance = constitution + level;
-                    damageMod = (intelligence - 10) * level;
+                    resistance = Scores.con + level;
+                    damageMod = (Scores.intel - 10) * level;
                     break;
                 case "t":
-                    dexterity = dexterity + 1;
-                    charisma = charisma + 1;
-                    adventuringClass = "Theif";
+                    Scores.dex = Scores.dex + 1;
+                    Scores.cha = Scores.cha + 1;
+                    Class.className = "Theif";
                     WriteLine("You are now a theif! Enemies shall fear your deadly daggers!");
-                    resistance = dexterity + level;
-                    damageMod = (charisma - 10) * level;
+                    resistance = Scores.dex + level;
+                    damageMod = (Scores.cha - 10) * level;
                     break;
                 default:
                     WriteLine("Ye dun goofed!");
                     goto AdventuringClass;
             }
-            WriteLine("Great! Your name is: {0} {1}. You race is: {2}. Your adventuring class is: {3}", nameFirst, nameLast, race, adventuringClass);
+            WriteLine("Great! Your name is: {0} {1}. You race is: {2}. Your adventuring class is: {3}", nameFirst, nameLast, Race.raceName, Class.className);
             WriteLine("You are now level 1!");
-            health = level * constitution;
+            health = level * Scores.con;
             WriteLine("Your health is {0}.", health);
             WriteLine("Your resistance is {0}.", resistance);
             WriteLine("Your damage modifier is {0}", damageMod);
